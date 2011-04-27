93b0392ddb0ca9d863256f916c1cb0aaca94064d
Alexandre Soares <alexandrejr_soares@yahoo.com.br>
2011-04-27 15:14:11 -0300

    Alteração setor de inscrições - 270411 15:00

--- db/migrate/20110427132006_add_tipo_participante_to_participantes.rb
+++
@@ -1,9 +0,0 @@
-class AddTipoParticipanteToParticipantes < ActiveRecord::Migration
-  def self.up
-    add_column :participantes, :tipo_participante, :integer, :default => 1
-  end
-    
-  def self.down
-    remove_column :participantes, :tipo_participante
-  end
-end
