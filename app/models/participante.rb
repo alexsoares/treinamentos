2fa76bf1f6d75d82758b2d2230cb32e76d017ab1
Manutencao <alexandrejr_soares@yahoo.com.br>
2012-08-09 08:20:22 -0300

    all

--- app/models/participante.rb
+++ app/models/participante.rb
@@ -8,8 +8,8 @@ class Participante < ActiveRecord::Base
   validates_presence_of :unidade_id, :if => :tipo? , :message => " É PREENCHIMENTO OBRIGATÓRIO"
   validates_presence_of     :email
   validates_length_of       :email,    :within => 6..100 #r@a.wk
-  #validates_uniqueness_of   :email
-  #validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
+  validates_uniqueness_of   :email
+  validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
   #validates :email, :if => :verifica_email?, :message => "Email precisa ser confirmado"
   attr_accessor :email_confirmation
 def before_save
